33812be083c134b605d6bd5b8f8d571f8c2afae2
Vsevolod Kukol <sevo@sevo.org>
2016-03-07 01:25:26 +0100

    Add Xwt.Drawing.Color visualizer

---
+++ Visualizers/ColorConverter.cs
@@ -0,0 +1,55 @@
+﻿//
+// ColorConverter.cs
+//
+// Author:
+//       Vsevolod Kukol <sevo@sevo.org>
+//
+// Copyright (c) 2016 Vsevolod Kukol
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+using System;
+using Mono.Debugging.Client;
+using MonoDevelop.Debugger;
+using Xwt.Drawing;
+
+namespace MonoDevelop.Xwt
+{
+	public class ColorConverter : DebugValueConverter<Color>
+	{
+		public override bool CanGetValue (ObjectValue val)
+		{
+			return val.TypeName != null && val.TypeName == "Xwt.Drawing.Color";
+		}
+
+		public override Color GetValue (ObjectValue val)
+		{
+			var ops = DebuggingService.DebuggerSession.EvaluationOptions.Clone ();
+			ops.AllowTargetInvoke = true;
+
+			//var raw = (RawValue)val.GetRawValue ();
+
+			return new Color (
+				(double)val.GetChild ("Red", ops).GetRawValue (ops),
+				(double)val.GetChild ("Green", ops).GetRawValue (ops),
+				(double)val.GetChild ("Blue", ops).GetRawValue (ops),
+				(double)val.GetChild ("Alpha", ops).GetRawValue (ops));
+		}
+	}
+}
+
